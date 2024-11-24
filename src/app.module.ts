40e51ca97bb616657d1cec01f323e50e8fcbf74f
vtmtea <vtmtea@gmail.com>
2024-11-24 15:52:29 +0800

    :sparkles: Add database and config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				type: "mysql",
+				host: configService.get<string>("DATABASE_HOST"),
+				port: configService.get<number>("DATABASE_PORT"),
+				username: configService.get<string>("DATABASE_USERNAME"),
+				password: configService.get<string>("DATABASE_PASSWORD"),
+				database: configService.get<string>("DATABASE_NAME"),
+				autoLoadEntities: true,
+			}),
+		}),
+	],
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
